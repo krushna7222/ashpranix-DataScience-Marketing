439b00e4914e1f4d6cf954a737e62eeac5fd32ba
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-11-21 13:40:36 +0530

    Update trainerData.ts

--- src/data/trainerData.ts
+++ src/data/trainerData.ts
@@ -20,11 +20,11 @@ export const trainerData: TrainerData = {
   title: "Senior Data Scientist & AI/ML Instructor",
   description:
     "Pravin Nagare Sir is an expert Data Scientist with deep experience in Python, Machine Learning, Deep Learning, Data Engineering, and Gen-AI. With 8+ years of experience in solving real-world data problems, he has trained 500+ students and guided many learners to secure roles in top MNCs and AI-driven companies.",
-  experience: "10+ Years Exp",
+  experience: "12+ Years Exp",
   image: trainerimg,
 
   achievements: [
-    { icon: Users, label: "500+ Students Mentored" },
+    { icon: Users, label: "25000+ Students Mentored" },
     { icon: BookOpen, label: "Real Industry Projects" },
     { icon: Award, label: "Top Rated Instructor" },
   ],
